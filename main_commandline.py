a3c54dc2de477317a53a0cad39c941327f25df4f
Maksim <115571274+Maks1m-Se@users.noreply.github.com>
2024-04-10 11:00:32 +0200

    Update

--- main_commandline.py
+++ main_commandline.py
@@ -17,9 +17,18 @@ def display_df(df, header=None):
     print('\n')
     print(f'Table: {header}')
     print(tabulate(df, "keys", tablefmt="github"))
+    print('\n')
 
+number_of_combs = len(list(itertools.combinations(df_tasks.Tasks, 2)))
+number_of_questions = number_of_combs * 3
 
+print("\n### Overview Tasks ###")
 display_df(df_tasks, "df_tasks")
+print("Count: ", df_tasks["Tasks"].nunique())
+print("Combinations: ", number_of_combs)
+print("Questions: ", number_of_questions)
+
+print('-----\n\n')
 
 IT_df, UT_df, ET_df = df_tasks.copy(), df_tasks.copy(), df_tasks.copy()
 IT_df["Importance_Score"] = 0
@@ -45,26 +54,32 @@ def eval_tasks(df):
     output: IT, UT, ET with scores
     '''
     
+    count_question = 0
 
     # create combination pairs of tasks
     for comb in itertools.combinations(df.Tasks, 2):
 
+        
+
         ## ask user for evaluation ##
 
         # Importance
-        choice = inquirer.list_input("Which is more important?", choices=comb)
+        count_question +=1
+        choice = inquirer.list_input(f"({count_question}/{number_of_questions}) Which is more important?", choices=comb)
         print(f"Choice: '{choice}' is more important.")
         IT_df.loc[df['Tasks'] == choice, 'Importance_Score'] = IT_df['Importance_Score'] + 1
         display_df(IT_df, "IT_df")
 
         # Urgency
-        choice = inquirer.list_input("Which is more urgent?", choices=comb)
+        count_question +=1
+        choice = inquirer.list_input(f"({count_question}/{number_of_questions}) Which is more urgent?", choices=comb)
         print(f"Choice: '{choice}' is more urgent.")
         UT_df.loc[df['Tasks'] == choice, 'Urgency_Score'] = UT_df['Urgency_Score'] + 1
         display_df(UT_df, "UT_df")
         
         # Effort
-        choice = inquirer.list_input("Which is more effortless?", choices=comb)
+        count_question +=1
+        choice = inquirer.list_input(f"({count_question}/{number_of_questions}) Which is more effortless?", choices=comb)
         print(f"Choice: '{choice}' is more effortless.")
         ET_df.loc[df['Tasks'] == choice, 'Effort_Score'] = ET_df['Effort_Score'] + 1
         display_df(ET_df, "ET_df")
@@ -88,12 +103,33 @@ def render_task_rank():
     for task in RT_df.Tasks:
 
         sum_task_score = 0
-        sum_task_score = IT_df.loc[IT_df['Tasks'] == task, 'Importance_Score']
+        sum_task_score += IT_df.loc[IT_df['Tasks'] == task, 'Importance_Score']
         sum_task_score += UT_df.loc[UT_df['Tasks'] == task, 'Urgency_Score']
-        sum_task_score +=  ET_df.loc[ET_df['Tasks'] == task, 'Effort_Score']
+        sum_task_score += ET_df.loc[ET_df['Tasks'] == task, 'Effort_Score']
 
         print(task, sum_task_score)
 
         RT_df.loc[RT_df['Tasks'] == task, 'Sum_of_Scores'] = sum_task_score
+
+    ## Create Ranks ##
+    # Importance
+    IT_df["Rank"] = IT_df["Importance_Score"].rank(ascending=False)
+    IT_df.sort_values("Rank", inplace = True)
+    # Urgency
+    UT_df["Rank"] = UT_df["Urgency_Score"].rank(ascending=False)
+    UT_df.sort_values("Rank", inplace = True)
+    # Effort
+    ET_df["Rank"] = ET_df["Effort_Score"].rank(ascending=False)
+    ET_df.sort_values("Rank", inplace = True)
+    # Result Summary
+    RT_df["Rank"] = RT_df["Sum_of_Scores"].rank(ascending=False)
+    RT_df.sort_values("Rank", inplace = True)
+
+    #Display
+    display_df(IT_df, "IT_df")
+    display_df(UT_df, "UT_df")
+    display_df(ET_df, "ET_df")
+    print("-----\n\n### Result Ranked Tasks ###")
+    display_df(RT_df, "RT_df")
 
 render_task_rank()
--- tasks.py
+++ tasks.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 DICT_TASKS = {
-    "A": "Wäsche",
-    "B": "Sport",
-    "C": "Geschirr",
-    #"D": "Wand streichen",
+    "A": "Aufräumen Boxen",
+    "B": "Putzen",
+    "C": "Wäsche",
+    "D": "Sport",
 }
 
 # Create a list of tuples with (task_label, task_description)
@@ -12,4 +12,4 @@ tasks_data = [(key, value) for key, value in DICT_TASKS.items()]
 
 # Create a DataFrame from the list of tuples
 df_tasks = pd.DataFrame(tasks_data, columns=['Label', 'Tasks'])
-
+df_tasks = df_tasks.set_index("Label")
