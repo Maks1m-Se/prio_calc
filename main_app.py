f589801df4b8599723836f35c0605d397938d5d4
Maksim <115571274+Maks1m-Se@users.noreply.github.com>
2024-05-14 23:01:57 +0200

    Meilenstein

--- main_app.py
+++ main_app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
+import os
 from tasks import df_tasks
 from WEBAPP_funcs import evaluate_tasks, compare_tasks, render_task_rank
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prio_calc.db'
@@ -14,13 +14,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-#### Tables ####
+#### Database Models ####
 # Task Table
 class Todo(db.Model):
     task_id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(200), nullable=False, unique=True)
     done=db.Column(db.Boolean)
     datetime_added = db.Column(db.DateTime, default=datetime.now())
+
     def __repr__(self):
         return f'<{self.task_id} {self.name} {self.done} {self.datetime_added}>'
     
@@ -31,6 +32,7 @@ class IT_db(db.Model):
     task_id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(200), nullable=False)
     Importance_Score=db.Column(db.Integer)
+
     def __repr__(self):
         return f'<{self.task_id} {self.name} {self.Importance_Score}>'
 
@@ -41,17 +43,33 @@ class IT_db(db.Model):
 
 # Results Table
 
-#Variables to pass to subdomains
+### Global Vars and Funcs ###
 attribute = "Importance"
+tasks_list = []
+first_task = ''
+pop_task = False
 
-
+#### Helper Functions ####
+def get_tasks_list():
+    """Query the Todo table and return a list of task names."""
+    tasks = Todo.query.order_by(Todo.task_id).all()
+    return [task.name for task in tasks]
 
 #### ROUTES ####
-
 # Main Page
 @app.route('/')
 def index():
+    global tasks_list, pop_task
+    print('INDEX') # Debugging
+
     todo_list = Todo.query.all()
+
+    pop_task = True
+    print('Pop on: ', pop_task) # Debugging
+
+    tasks_list = get_tasks_list()
+    print('Task List:\n', tasks_list) # Debugging
+    
     return render_template('index_WEBAPP.html', title='Main Page',
                            dataframe=df_tasks, todo_list=todo_list)
 
@@ -59,6 +77,7 @@ def index():
 @app.route('/return_to_index', methods=['POST'])
 def return_to_index():
     return render_template('index_WEBAPP.html', title='Main Page')
+
 
 # Add Task
 @app.route('/add_task', methods=['POST'])
@@ -93,30 +112,62 @@ def delete_task(todo_id):
 # Evalute Tasks
 @app.route('/evaluate_tasks', methods=['GET', 'POST'])
 def evaluate_tasks_route():
+    global tasks_list, first_task
+    print('EVALUATE') # Debugging
+    print('Pop on: ', pop_task) # Debugging
+
+    if not pop_task:
+        tasks_list = get_tasks_list()
+    
+    print('Task List:\n', tasks_list) # Debugging
+
+    # get first task
+    if tasks_list:
+        first_task = tasks_list[0]
+
     IT_df, UT_df, ET_df = evaluate_tasks(df_tasks)
     IT_list = IT_db.query.all()
+
     return render_template('evaluate.html', title='Evaluate Page',
                            IT_df=IT_df, UT_df=UT_df, ET_df=ET_df,
-                           IT_list=IT_list,
+                           IT_list=IT_list, first_task=first_task,
                            attribute=attribute)
 
 # Update Scores
 @app.route('/update_scores', methods=['GET', 'POST'])
 def update_scores():
+    global tasks_list, first_task, pop_task
+    print('UPDATE') # Debugging
+
     IT_value = request.form.get("IT_value")
-    if IT_value is None:
-        IT_value = 0
-    else:
-        IT_value = int(IT_value)  # Convert to integer
-    task = IT_db.query.filter_by(name="Task A").first()
+    IT_value = int(IT_value) if IT_value is not None else 0  # Convert to integer
+    
+    task = IT_db.query.filter_by(name=first_task).first()
     if task.Importance_Score is None:
         task.Importance_Score = 0  # Ensure Importance_Score is not None
     task.Importance_Score += IT_value
     db.session.commit()
+
+    pop_task = True
+    print('Pop on: ', pop_task) # Debugging
+
+    # pop first task
+    if tasks_list:
+        removed_task = tasks_list.pop(0)
+        print('Pop: ', removed_task) # Debugging
+
+    # get new first task
+    if not tasks_list:
+        pass # or first_task = "--- (no tasks left)"
+    else: 
+        first_task = tasks_list[0]
+    print('Task List:\n', tasks_list) # Debugging
+    
+
     return redirect(url_for("evaluate_tasks_route"))    
 
 #### END ROUTES ####
 
-
+#### Main ####
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
